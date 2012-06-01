3704cd1e9b383fbe17be659dac184cc058cf2d41
Stephen Chen <stephenchen0@gmail.com>
2012-06-01 18:16:17 -0400

    Resolved commit conflicts

--- a1/simulator.cpp
+++ a1/simulator.cpp
@@ -38,7 +38,7 @@ void Start_simulation (int ticks) {
     }
 }
 
-<<<<<<< HEAD
+
 void Compute_performances () {
     /*Calculate and display the results such as average number of packets in queue, average delay in queue and idle time for the server. */
 }
@@ -84,27 +84,7 @@ int main(int argc, char* argv[]) {
 	           usage(argv);
 	}
 
-
-
     /*Initialise important terms such as t_arrival = exponential r.v, # of pkts in queue = 0, t_departure = t_arrival ( this implies that first time departure will be called as soon as a packet arrives in the queue*/
-=======
-void Arrival ( int t ) {
-    /* Generate a packet as per the exponential distribution and insert the packet in the queue (an array or a linked list)*/ 
-    
-    
-    
-}
-
-int Departure ( int t ) {
-    /* Check the queue for the packet, if head of the queue is empty, return 0 else if the queue is non-empty delete the packet from the queue after an elapse of the deterministic service time. */
-    
-    if(packets->empty()) {
-        return 0;
-    } else {
-        
-        
-    }
->>>>>>> test
     
     packets = new queue<int>();
     
