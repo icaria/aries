1721ddfeb453a662f47fb87120e84c989c2c6672
U-G3-NOTEPAD\Gabriella <Gabriella@G3-NOTEPAD.(none)>
2012-06-02 20:00:47 -0400

    switch to long long data type

--- a1/simulator.cpp
+++ a1/simulator.cpp
@@ -1,4 +1,4 @@
--//  ECE358 Lab 1
+//  ECE358 Lab 1
 //  simulator.cpp
 //
 //  Created by Stephen Chen and Gabriella Grandilli on 12-05-29.
@@ -12,16 +12,21 @@
 #include <cstdlib>
 using namespace std;
 
-queue<int> packets;
-int TICKS = 5000; //5000000000
+struct packet
+{
+    long long arrivalTime;
+};
 
-void Arrival ( int t ) {
+queue<packet> packets;
+long long TICKS = 5000; //5000000000
+
+void Arrival ( long long t ) {
     // Generate a packet as per the exponential distribution 
 	// insert the packet in the queue (an array or a linked list)*/ 
 	//queue.push(packet);
 }
 
-int Departure ( int t ) {
+int Departure ( long long t ) {
     
 	// If the head of the queue is empty/the queue is empty
     if(packets.empty()) {
@@ -34,9 +39,10 @@ int Departure ( int t ) {
 	}
 }
 
-void Start_simulation (int ticks) {
+void Start_simulation (long long ticks) {
     
-    for (int t=1; t<= ticks; t++) {
+    long long t = 0;
+    for (t=1; t<= ticks; t++) {
         Arrival(t);
         Departure(t);
     }
@@ -71,17 +77,26 @@ void usage( char *argv[] ) {
 }
 
 int main(int argc, char* argv[]) {
-	int n, lamda, l, c, k;
+	int lamda; // number packets generated per number arrived (packets per second)
+    int l; //length
+    int c; // transmission rate of the output link (bits per second)
+    int n;
+    int k; // size of the buffer (number of packets); if not specified, infinite
+    
+    bool isK = false;
 
 	switch(argc) {
 		case 5: // M/D/1/K
-		   if (!convert(k, argv[4])) 
-                   usage(argv);
+            if (!convert(k, argv[4])) 
+            {
+                usage(argv);
+            }
+            isK = true;
 		case 4:
-		   if (!convert(k, argv[3])) {                   
+            if (!convert(k, argv[3])) {                   
                usage(argv);
             }
-           break;
+            break;
         case 3:         // two options is invalid
         case 2:         // one option is invalid
         case 1:         // zero options is invalid
