5d8cd63607340fb1c11b2dfd09753d36c5cfdcb6
Stephen Chen <stephenchen0@gmail.com>
2012-07-08 17:16:41 -0400

    19

--- a2/lab2.c
+++ a2/lab2.c
@@ -25,8 +25,8 @@ void Sender(Event Current_Event) {
         } else if(Current_Event.Type == RECEIVE_ACK) {
             if(Queue_Head != NULL) {
             //if(Current_Event.Type == TIMEOUT) {
-                Current_Event.Seq_Num = (Current_Event.Seq_Num + 1) % 2;
-                Current_Event.Pkt_Num = Current_Event.Pkt_Num + 1;
+                Current_Event.Seq_Num = (Queue_Head->Seq_Num + 1) % 2;
+                Current_Event.Pkt_Num = Queue_Head->Pkt_Num + 1;
             }
                 if(Current_Event.Pkt_Num != N) {
                     Channel(SEND_FRAME, Current_Event.Seq_Num, Current_Event.Pkt_Num, Current_Event.Time);
